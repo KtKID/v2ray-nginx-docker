23f0a84cbd2903de3de7334b3e62d8d690384948
KtKID <649693008@qq.com>
2022-12-21 14:58:00 +0000

    支持修改配置后不用rebuild重新运行

--- build_run.sh
+++ build_run.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
     # -e V2RAY_TOKEN="0000000-0000-0000-000-000000" \
-docker build -t docker-nginx-v2ray .
+container_name="nginx-v2ray"
+if [ "$(docker ps -q -f name=${container_name})" ];
+then
+    echo "${container_name} exist and will restart in 5s"
+    [ "$(docker restart -t 5 ${container_name})" ]
+else
+    echo "${container_name} not exist"
 
-SITE_DOMAIN="domain" #你的域名
-SSL_PORT=13431
-V2RAY_PORT=54321
-docker container run \
-    -d \
-    --name nginx-v2ray \
-    -p 80:80 \
-    -p ${V2RAY_PORT}:${V2RAY_PORT} \
-    -p ${SSL_PORT}:${SSL_PORT} \
-    -v "/etc/letsencrypt/archive/${SITE_DOMAIN}":/data/cert/${SITE_DOMAIN} \
-    -e SITE_DOMAIN="${SITE_DOMAIN}" \
-    -e V2RAY_TOKEN="0000000-0000-0000-000-000000" \
-    -e V2RAY_PORT=${V2RAY_PORT} \
-    -e SSL_PORT=${SSL_PORT} \
-    -e V2RAY_WS_PATH=/ray \
-    docker-nginx-v2ray
+    docker build -t docker-nginx-v2ray .
+
+    SITE_DOMAIN="domain" #你的域名
+    SSL_PORT=13431
+    V2RAY_PORT=54321
+    docker container run \
+        -d \
+        --name nginx-v2ray \
+        -p ${V2RAY_PORT}:${V2RAY_PORT} \
+        -p ${SSL_PORT}:${SSL_PORT} \
+        -v "/etc/letsencrypt/archive/${SITE_DOMAIN}":/data/cert/${SITE_DOMAIN} \
+        -e SITE_DOMAIN="${SITE_DOMAIN}" \
+        -e V2RAY_TOKEN="0000000-0000-0000-000-000000" \
+        -e V2RAY_PORT=${V2RAY_PORT} \
+        -e SSL_PORT=${SSL_PORT} \
+        -e V2RAY_WS_PATH=/king \
+        docker-nginx-v2ray
+fi
