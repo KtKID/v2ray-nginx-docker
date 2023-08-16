8e2385088acfa9b89a69ee12bfd7165cf302e6d9
KtKID <649693008@qq.com>
2023-08-16 20:53:57 +0800

    token定义变量

--- build_run.sh
+++ build_run.sh
@@ -14,6 +14,8 @@ else
     SSL_PORT_START=10000    #start-end受限于nginx最大连接数量
     SSL_PORT_END=10010
     V2RAY_PORT=54321
+    TOKEN="0000000-0000-0000-000-000000"
+    WS_PATH="/xxx"
 
     port_flags=""
     for((i=$SSL_PORT_START; i<$SSL_PORT_END; i++))
@@ -29,10 +31,10 @@ else
         -p ${V2RAY_PORT}:${V2RAY_PORT} \
         -v "/etc/letsencrypt/archive/${SITE_DOMAIN}":/data/cert/${SITE_DOMAIN} \
         -e SITE_DOMAIN="${SITE_DOMAIN}" \
-        -e V2RAY_TOKEN="0000000-0000-0000-000-000000" \
+        -e V2RAY_TOKEN=${TOKEN} \
         -e V2RAY_PORT=${V2RAY_PORT} \
         -e SSL_PORT_START=${SSL_PORT_START} \
         -e SSL_PORT_END=${SSL_PORT_END} \
-        -e V2RAY_WS_PATH=/xxx \
+        -e V2RAY_WS_PATH=${WS_PATH} \
         docker-nginx-v2ray
 fi
